73b321e7d6f48d007025bb0ba71cd6aa423b81f0
Logan Bias <loganbiasomma@gmail.com>
2016-08-08 17:58:57 +0600

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-const PORT = 8080;//process.env.PORT
+const PORT = process.env.PORT;//8080;
 
 var Client = require('node-rest-client').Client;
  
